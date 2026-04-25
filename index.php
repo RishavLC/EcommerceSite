b79ea77706d7fd873e07d5558489ae854f230608
RishavLC <shrestharishav444@gmail.com>
2026-04-25 21:27:10 +0545

    footer added

--- index.php
+++ index.php
@@ -174,6 +174,9 @@
 
     </div>
     </div>
+    <div class="footer">
+        &copy @ copyright 2026
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
