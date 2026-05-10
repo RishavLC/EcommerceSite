429e584b0404c7ac925be7aea25f7ad1bdcc97a6
RishavLC <shrestharishav444@gmail.com>
2026-05-10 21:55:29 +0545

    backend started

--- index.php
+++ index.php
@@ -265,6 +265,7 @@
       <p class="mb-0">&copy; 2026 e-commerce. All rights reserved.</p>
     </div>
   </div>
+  <!-- now i wnt to add every backend component -->
 </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
