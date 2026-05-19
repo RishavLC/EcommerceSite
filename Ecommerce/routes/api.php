283068eb21fa1ac8a20f291b3c9cf1f5ad91c7b2
RishavLC <shrestharishav444@gmail.com>
2026-05-19 22:01:52 +0545

    first api created with laravel Sanctum

---
+++ Ecommerce/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+// Public Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected Routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+});
